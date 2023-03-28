0d2c3544a737029f4cd49192e95a33b4bc7d41a8
ahmaruff <ahmadmaruf2701@gmail.com>
2023-03-28 07:21:16 +0700

    fix header typo

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
 <header>
-    <h1>Chiper Text Generator</h1>
+    <h1>Cipher Text Generator</h1>
     <h4>Layanan enkripsi <em>plaintext</em> menggunakan aturan Caesar Cipher Sederhana</h4>
 </header>
