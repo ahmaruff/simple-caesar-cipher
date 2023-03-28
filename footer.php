d17c1aeaefd2e5411fbafbdaf2290583178e2a57
ahmaruff <ahmadmaruf2701@gmail.com>
2023-03-28 07:30:50 +0700

    add github repo and thanks to

--- footer.php
+++ footer.php
@@ -1,3 +1,8 @@
 <footer>
-    <p>Ahmad Ma'ruf - 2023</p>
+    <p>
+        Ahmad Ma'ruf - 2023 |
+        <a href="https://github.com/ahmaruff/simple-caesar-cipher">Github</a>
+    </p>
+    
+    <p>thanks to <a href="https://www.programmingalgorithms.com/algorithm/caesar-cipher/php/">ProgrammingAlgorithms</a></p>
 </footer>
